4c9eee1559573f4fdf57a28fdbf7beb70cc8c2c9
Kanishk Gupta <kanishk333gupta@gmail.com>
2021-10-01 21:38:23 +0530

    Initial commit
    Signed-off-by: Kanishk Gupta <kanishk333gupta@gmail.com>

--- gfg/Linked List Insertion At Position.cpp
+++ gfg/Linked List Insertion At Position.cpp
@@ -3,16 +3,17 @@
 void insertAtPosition(Node *head, int pos, int data)
 {
     //Your code here
-    
     Node * temp =new Node(data) ;
-  
+    if(pos==1) {
+        temp->next = head ;
+    }
     Node * curr = head ;
-    for(int i = 0 ; i <= pos-2 && curr!=NULL; i++){
+    for(int i = 0 ; i <pos-1 && curr!=NULL; i++){
         curr = curr->next ;
     }
     if (curr == NULL) return ;
-    
     temp -> next = curr->next ;
     curr->next = temp ;
     return  ;
+
 }
