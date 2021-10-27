ba918d0210859de9d093f15103a0098e2256451a
kanishk333gupta <81621992+kanishk333gupta@users.noreply.github.com>
2021-10-27 23:20:17 +0530

    Delete gfg  & leetcode directory

--- gfg  & leetcode/Factorial.cpp
+++
@@ -1,9 +0,0 @@
-#https://practice.geeksforgeeks.org/problems/factorial5739/1#
-
-long long int factorial(int N){
-       //code here
-       long long int ans = 1;
-       for(int i = 1 ; i < N ; i++ ){
-          ans = ans*(i+1) ;  
-       }return ans ;
-}
--- gfg  & leetcode/Find nCr.cpp
+++
@@ -1,8 +0,0 @@
-#https://practice.geeksforgeeks.org/problems/find-ncr/1/#
-
-int nCr(int n,int r)
-{
-    //Your code here
-    if(n==r || r==0) return 1 ;
-    return nCr(n-1,r-1)+nCr(n-1,r);
-}
