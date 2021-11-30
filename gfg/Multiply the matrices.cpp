cdb89928320d98d09f17b70403a41bc69b3f829e
Kanishk Gupta <kanishk333gupta@gmail.com>
2021-11-30 20:43:05 +0530

    Initial commit
    Signed-off-by: Kanishk Gupta <kanishk333gupta@gmail.com>

---
+++ gfg/Multiply the matrices.cpp
@@ -0,0 +1,18 @@
+#https://practice.geeksforgeeks.org/problems/multiply-the-matrices-1587115620/1#
+
+vector<vector<int> > multiplyMatrix( const vector<vector<int> >& A, const vector<vector<int> >& B)
+    {
+        // code here
+       vector<vector<int>> vec;
+       if(A[0].size()!=B.size())return vec;
+       for(int j=0;j<A.size();j++){
+           vector<int> t;
+           for(int i=0 ; i<B[0].size();i++){
+               int temp = 0;
+               for(int c=0;c<B.size();c++){
+                   temp+=A[j][c]*B[c][i];
+               }t.push_back(temp);
+           }vec.push_back(t);
+       }return vec;
+    }
+};
